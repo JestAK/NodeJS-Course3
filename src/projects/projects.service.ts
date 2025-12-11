ffe790a0c5d5fff290fd098ad32789ee320c2a8f
Kaliberda Anton <73352390+JestAK@users.noreply.github.com>
2025-12-11 03:09:38 +0100

    Fix IDE type error

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from '@nestjs/common';
-import {CreateProjectDto} from './dto/CreateProject.dto';
-import {PrismaService} from '../prisma.service';
-import {Project} from "@prisma/client";
+import { Injectable } from '@nestjs/common';
+import { CreateProjectDto } from './dto/CreateProject.dto';
+import { PrismaService } from '../prisma.service';
+import { Project } from '@prisma/client';
 
 @Injectable()
 export class ProjectsService {
